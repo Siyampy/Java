684cdd853d42c0e001c6d07e27e8328a1ae3eaba
Siyam <dhanush0206@gmail.com>
2019-06-26 21:55:27 +0530

    Map Introduction

---
+++ Core_Java/com/collections/Map/Entry_.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.collections.Map;
+
+/**
+ * @author Siyam
+ *
+ */
+public class Entry_ 
+{
+//	InnerIf of the Map If
+//	Without Map Entry is no vailable
+//	if MAp
+//	{
+//		if Entry
+//		{
+//	Object  getKey();
+//	Object getValue();
+//	Object setValue(Object new_VAlue);
+//			
+	
+//	These methods are applied only on the Entry Objects
+//		}
+//	}
+
+}
---
+++ Core_Java/com/collections/Map/Map_.java
@@ -0,0 +1,32 @@
+package com.collections.Map;
+
+public class Map_ 
+{
+//	To rep the group of objects in the key-value pairs
+//	Each k-v pair is an entry
+//	it has its own methods
+	
+//	put(ob1,ob2)
+//	Object put(ob1,ob2)
+//	putAll(Map m)
+//	m.get(key)
+//	Object remove(key)
+//	containsKey(object key)
+//	containsValue(Object value)
+//	bool isEmpty()
+//	int size()
+//	void clear()
+//	Set keySet()------To get only keys
+//	Collection values()----To get only values
+//	Set entrySet()------To get theset ofthe entry objects
+	
+	
+//	put(ob1,ob2)
+//	It simply adds thek-v pair into the map
+//	Object put(ob1,ob2)
+//	It replaces the duplicate ;key and returns the old values 
+//	putAll(Map m)
+//	m.get(key)--returns value
+	
+
+}
