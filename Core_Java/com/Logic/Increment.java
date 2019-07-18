41260eb1045ca795a067c5e673d6b0183a96830b
Siyam <dhanush0206@gmail.com>
2019-07-18 18:27:48 +0530

    Increment post,pre logic

---
+++ Core_Java/com/Logic/Increment.java
@@ -0,0 +1,13 @@
+package com.Logic;
+
+public class Increment {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a=7;
+		System.out.println(a+ a++ +a);
+
+	}
+
+}
